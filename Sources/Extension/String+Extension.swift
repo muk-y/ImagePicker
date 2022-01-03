e2d6ca95d7e74edb5dca6ad561b76f2948c40d22
ekmacmini43 <ekmacmini43@ekmacmini43s-Mac-mini.local>
2022-01-03 18:13:29 +0545

    Bundle code refactored

--- Sources/Extension/String+Extension.swift
+++ Sources/Extension/String+Extension.swift
@@ -11,6 +11,7 @@ extension String {
 
     var localized: String {
         let path = Bundle.module.path(forResource: Locale.current.languageCode, ofType: "lproj")
+        print("Current package language: \(Locale.current.languageCode)")
         let bundle = Bundle(path: path!)!
         return NSLocalizedString(self, bundle: bundle, comment: "")
     }
