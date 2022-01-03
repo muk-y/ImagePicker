5c07fc9e2e82d83ec961e0ef61c5a73e7e3212c5
ekmacmini43 <ekmacmini43@ekmacmini43s-Mac-mini.local>
2022-01-03 18:24:10 +0545

    Code refactor

--- Sources/Extension/String+Extension.swift
+++ Sources/Extension/String+Extension.swift
@@ -10,8 +10,9 @@ import Foundation
 extension String {
 
     var localized: String {
-        let path = Bundle.module.path(forResource: Locale.current.languageCode, ofType: "lproj")
+        let path = Bundle.module.path(forResource: "ja", ofType: "lproj")
         print("Current package language: \(Locale.current.languageCode)")
+        print("Current device language in package: \(UserDefaults.standard.string(forKey: "LCLCurrentLanguageKey"))")
         let bundle = Bundle(path: path!)!
         return NSLocalizedString(self, bundle: bundle, comment: "")
     }
